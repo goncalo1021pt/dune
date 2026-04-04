f3626755502dd5d593fc2e70f23172bc3a062c09
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2026-04-04 02:24:42 +0100

    Finished Map

--- srcs/map.cpp
+++ srcs/map.cpp
@@ -22,11 +22,11 @@ void GameMap::addTerritory(const std::string& name, terrainType terrain, int spi
 void GameMap::initializeMap() {
 	territories.clear();
 
-	addTerritory("Arrakeen",        terrainType::city, 0, {10});
-	addTerritory("Carthag",         terrainType::city, 0, {11});
+	addTerritory("Arrakeen",        terrainType::city, 2, {10});
+	addTerritory("Carthag",         terrainType::city, 2, {11});
 	addTerritory("Sietch Tabr",     terrainType::city, 0, {14});
 	addTerritory("Habbanya Sietch", terrainType::city, 0, {17});
-	addTerritory("Tuek's Sietch",   terrainType::city, 0, {5});
+	addTerritory("Tuek's Sietch",   terrainType::city, 1, {5});
 
 	getTerritory("Arrakeen")->hasTransporter  = true;
 	getTerritory("Arrakeen")->specialMovement = true;
@@ -41,35 +41,35 @@ void GameMap::initializeMap() {
 	addTerritory("Plastic Basin",    terrainType::rock, 0, {12, 13, 14});
 	addTerritory("False Wall West",  terrainType::rock, 0, {16, 17, 18});
 	
-	addTerritory("Cielago West",       terrainType::desert, 0,  {18, 1});
-	addTerritory("Meridian",           terrainType::desert, 0,  {1, 2});
-	addTerritory("Cielago Depression", terrainType::desert, 0,  {1, 2, 3});
-	addTerritory("Cielago North",      terrainType::desert, 8,  {1, 2, 3});
+	addTerritory("Cielago West",       terrainType::desert,  0, {18, 1});
+	addTerritory("Meridian",           terrainType::desert,  0, {1, 2});
+	addTerritory("Cielago Depression", terrainType::desert,  0, {1, 2, 3});
+	addTerritory("Cielago North",      terrainType::desert,  8, {1, 2, 3});
 	addTerritory("Cielago South",      terrainType::desert, 12, {2, 3});
-	addTerritory("Cielago East",       terrainType::desert, 0,  {3, 4});
+	addTerritory("Cielago East",       terrainType::desert,  0, {3, 4});
 	addTerritory("Harg Pass",          terrainType::desert,  0, {4, 5});
-	addTerritory("South Mesa",         terrainType::desert, 0,  {4, 5, 6});
-	addTerritory("The Minor Erg",      terrainType::desert,  0, {5, 6, 7, 8});
+	addTerritory("South Mesa",         terrainType::desert, 10, {4, 5, 6});
+	addTerritory("The Minor Erg",      terrainType::desert,  8, {5, 6, 7, 8});
 	addTerritory("Red Chasm",          terrainType::desert,  8, {7});
 	addTerritory("Gara Kulon",         terrainType::desert,  0, {8});
 	addTerritory("Basin",              terrainType::desert,  0, {9});
 	addTerritory("Hole in the Rock",   terrainType::desert,  0, {9});
-	addTerritory("Sihaya Ridge",       terrainType::desert,  0, {9});
+	addTerritory("Sihaya Ridge",       terrainType::desert,  6, {9});
 	addTerritory("Imperial Basin",     terrainType::desert,  0, {9, 10, 11});
 	addTerritory("Old Gap",            terrainType::desert,  6, {9, 10, 11});
 	addTerritory("Arsunt",             terrainType::desert,  0, {11, 12});
-	addTerritory("Broken Land",        terrainType::desert,  0, {11, 12});
+	addTerritory("Broken Land",        terrainType::desert,  8, {11, 12});
 	addTerritory("Tsimpo",             terrainType::desert,  0, {11, 12, 13});
 	addTerritory("Hagga Basin",        terrainType::desert,  6, {12, 13});
-	addTerritory("Rock Outcroppings",  terrainType::desert, 6,  {13, 14});
-	addTerritory("Bight of the Cliff", terrainType::desert, 0,  {14, 15});
-	addTerritory("Wind Pass",          terrainType::desert, 0,  {14, 15, 16, 17});
+	addTerritory("Rock Outcroppings",  terrainType::desert,  6, {13, 14});
+	addTerritory("Bight of the Cliff", terrainType::desert,  0, {14, 15});
+	addTerritory("Wind Pass",          terrainType::desert,  0, {14, 15, 16, 17});
 	addTerritory("The Great Flat",     terrainType::desert, 10, {15});
-	addTerritory("Funeral Plain",      terrainType::desert, 0,  {15});
-	addTerritory("The Greater Flat",   terrainType::desert, 0,  {16});
-	addTerritory("Habbanya Erg",       terrainType::desert, 8,  {16, 17});
-	addTerritory("Wind Pass North",    terrainType::desert, 6,  {17, 18});
-	addTerritory("Habbanya Ridge Flat",terrainType::desert, 0,  {17, 18});
+	addTerritory("Funeral Plain",      terrainType::desert,  0, {15});
+	addTerritory("The Greater Flat",   terrainType::desert,  0, {16});
+	addTerritory("Habbanya Erg",       terrainType::desert,  8, {16, 17});
+	addTerritory("Wind Pass North",    terrainType::desert,  6, {17, 18});
+	addTerritory("Habbanya Ridge Flat",terrainType::desert, 10, {17, 18});
 
 	addTerritory("Polar Sink", terrainType::northPole, 0,
 		{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18});
